29089a6acdac4626721cc84fe10fb163421772c2
System Administrator <root@c02qw1ueg8wl.corp.zuora.com>
2016-07-15 16:46:54 -0700

    stuff

--- app.rb
+++ app.rb
@@ -174,7 +174,6 @@ post "/postuser" do
 	headers 'Access-Control-Allow-Headers' => 'Authorization,Accepts,Content-Type,X-CSRF-Token,X-Requested-With'
 	headers 'Access-Control-Allow-Methods' => 'GET,POST,PUT,DELETE,OPTIONS'
 	headers 'Access-Control-Allow-Credentials' => 'true'
-	p request.body.read
 	payload = JSON.parse(request.body.read)
 	User.create(name: payload["name"], role: payload["role"], number_of_up_votes: payload["number_of_up_votes"], number_of_down_votes: payload["number_of_down_votes"])
 	returnmessage = 'success'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715210543) do
+ActiveRecord::Schema.define(version: 20160715234521) do
 
   create_table "categories", force: :cascade do |t|
     t.string  "name"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20160715210543) do
     t.string  "name"
     t.string  "role"
     t.integer "number_of_down_votes"
+    t.integer "number_of_up_votes"
   end
 
 end
