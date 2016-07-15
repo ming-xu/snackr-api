89dca9afc58c1e4838db043c4d083c1b17e2f278
System Administrator <root@c02qw1ueg8wl.corp.zuora.com>
2016-07-15 14:31:02 -0700

    allow cors

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ options "*" do
 
   # Needed for AngularJS
   response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
+  response.headers["Access-Control-Allow-Origin"] = "*"
 
   halt HTTP_STATUS_OK
 end
